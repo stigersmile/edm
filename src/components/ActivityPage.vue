0c394fdcbc5a85af451fe1dd5bb4499944f00b59
peter <stigersmile@gmail.com>
2022-06-23 23:16:59 +0800

    template

---
+++ src/components/ActivityPage.vue
@@ -0,0 +1,91 @@
+<template>
+
+<div>
+        <section class="activityBox" v-for= "(elm,index) in activityInfo" v-bind:key="index">
+          <mj-text
+            align="left"
+            color="#55575d"
+            font-family="Arial, sans-serif"
+            font-size="13px"
+            line-height="22px"
+            padding-bottom="0px"
+            padding-left="40px"
+            padding-right="40px"
+            padding-top="0px"
+            padding="10px 25px"
+          >
+          
+            <img :src="picture">
+            <p
+              style="
+                margin: 10px 0;
+                color: #151e23;
+                font-size: 16px;
+                font-family: Georgia, Helvetica, Arial, sans-serif;
+              "
+            >
+            
+              <b>{{elm.title}}</b>
+              <p>{{elm.introduction}}</p>
+              <br />✨課程時間&amp;內容
+              <br />{{elm.content}}
+              <br />🔺時間:{{elm.time}}
+              <br />🔺地點:{{elm.location}}
+            
+          </mj-text>
+
+          <mj-section
+            background-color="#ffffff"
+            background-repeat="repeat"
+            padding-bottom="0px"
+            padding="20px 0"
+            text-align="center"
+            vertical-align="top"
+          >
+            <mj-column>
+              <mj-button
+                align="center"
+                background-color="#354552"
+                border-radius="3px"
+                color="#ffffff"
+                font-family="Georgia, Helvetica, Arial, sans-serif"
+                font-size="14px"
+                font-weight="normal"
+                inner-padding="10px 25px"
+                padding="10px 25px"
+                text-decoration="none"
+                v-bind:href= "register"
+                text-transform="none"
+                vertical-align="middle"
+                >報名</mj-button
+              >
+            </mj-column>
+          </mj-section>
+
+          <button class="buttonStyle" @click="remove(index)">delete</button> 
+        </section>
+</div>
+</template>
+
+<script>
+export default {
+    props:["activityInfo"],
+    methods:{
+         remove(index){
+            this.$emit('delete',index); 
+        }
+    }
+}
+</script>
+
+<style scoped>
+.activityBox {
+  border: 1px solid black;
+}
+
+.buttonStyle{
+  width: 100%;
+  margin: 16px auto;
+  display: block;
+}
+</style>
--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -14,14 +14,14 @@
             <label for="activity-introduction">Introduction:</label>
             <textarea rows="4" cols="50" v-model="introduction">...</textarea>
 
-            <!-- <label for="activity-content">Content:</label>
-            <textarea rows="4" cols="50" v-model="content">...</textarea>  -->
-
             <label for="activity-time">Choose a activity time:</label>
             <input type="datetime-local" v-model="time" />
 
             <label for="activity-location">Location:</label>
             <input type="text" v-model="location" />
+
+            <label for="activity-register">Register:</label>
+            <input type="text" v-model="register" />
 
             <button class="buttonStyle" >add</button>
               <vue-editor v-model="HTMLcontent"></vue-editor>
@@ -30,72 +30,9 @@
         <!-- VIEW -->
         <div class="view">
 
-        <section class="activityBox" v-for= "(elm,index) in activityArr" v-bind:key="index">
-          <mj-text
-            align="left"
-            color="#55575d"
-            font-family="Arial, sans-serif"
-            font-size="13px"
-            line-height="22px"
-            padding-bottom="0px"
-            padding-left="40px"
-            padding-right="40px"
-            padding-top="0px"
-            padding="10px 25px"
-          >
-          
-            <img :src="picture">
-            <p
-              style="
-                margin: 10px 0;
-                color: #151e23;
-                font-size: 16px;
-                font-family: Georgia, Helvetica, Arial, sans-serif;
-              "
-            >
-            
-              <b>{{elm.title}}</b>
-              <p>{{elm.introduction}}</p>
-              <br />✨課程時間&amp;內容
-              <br />{{elm.content}}
-              <br />🔺時間:{{elm.time}}
-              <br />🔺地點:{{elm.location}}
-            
-          </mj-text>
-
-          <mj-section
-            background-color="#ffffff"
-            background-repeat="repeat"
-            padding-bottom="0px"
-            padding="20px 0"
-            text-align="center"
-            vertical-align="top"
-          >
-            <mj-column>
-              <mj-button
-                align="center"
-                background-color="#354552"
-                border-radius="3px"
-                color="#ffffff"
-                font-family="Georgia, Helvetica, Arial, sans-serif"
-                font-size="14px"
-                font-weight="normal"
-                inner-padding="10px 25px"
-                padding="10px 25px"
-                text-decoration="none"
-                href="https://www.surveycake.com/s/3p7nr"
-                text-transform="none"
-                vertical-align="middle"
-                >報名</mj-button
-              >
-            </mj-column>
-          </mj-section>
-
-          <button class="buttonStyle" @click="remove(index)">delete</button> 
-        </section>
-
-      <!-- view -->
-      </div>
+          <activity-page :activity-info="activityArr" @delete="remove(index)"></activity-page>
+        
+        </div>
 
 <!-- window -->          
 </div> 
@@ -104,9 +41,12 @@
 
 <script>
 import { VueEditor } from "vue2-editor";
+import ActivityPage from "./ActivityPage.vue";
+
 export default {
     components:{
-       VueEditor
+       VueEditor,
+       ActivityPage
     },
      data(){
        return{
@@ -118,6 +58,7 @@ export default {
             // content:"",
             picture:"",
             activityArr:[],
+            register:"",
             HTMLcontent: ""
        }         
      },
@@ -129,8 +70,8 @@ export default {
                  time:this.time,
                  location:this.location,
                  introduction:this.introduction,
-                //  content:this.content,
                  content : this.HTMLcontent,
+                 register: this.register,
                  picture:this.picture,
             }
             this.activityArr.push(activity)
